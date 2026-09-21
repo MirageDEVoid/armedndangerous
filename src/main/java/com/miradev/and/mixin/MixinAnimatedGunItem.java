26a89e174c145a59babc624a277f2c74ceace0e9
MirageDEVoid <miragedevoidbusiness@gmail.com>
2026-09-21 07:57:19 +0700

    Fixes:
    * Fixed a critical crash with MixinExtras
    * Changed @WrapOperation back to vanilla Mixin's @Redirect

--- src/main/java/com/miradev/and/mixin/MixinAnimatedGunItem.java
+++ src/main/java/com/miradev/and/mixin/MixinAnimatedGunItem.java
@@ -1,10 +1,9 @@
 package com.miradev.and.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.miradev.and.animations.ANDGunAnimations;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animation.AnimationController;
 import ttv.migami.jeg.item.AnimatedGunItem;
@@ -12,7 +11,7 @@ import ttv.migami.jeg.item.AnimatedGunItem;
 @Mixin(AnimatedGunItem.class)
 public abstract class MixinAnimatedGunItem {
 
-    @WrapOperation(
+    @Redirect(
             method = "registerControllers(Lsoftware/bernie/geckolib/core/animation/AnimatableManager$ControllerRegistrar;)V",
             at = @At(
                     value = "INVOKE",
@@ -20,8 +19,7 @@ public abstract class MixinAnimatedGunItem {
             ),
             remap = false
     )
-    private AnimationController<GeoAnimatable> and$replaceAnimationController(
-            AnimatedGunItem item, Operation<AnimationController<GeoAnimatable>> original) {
+    private AnimationController<GeoAnimatable> and$replaceAnimationController(AnimatedGunItem item) {
         return ANDGunAnimations.animationController(item);
     }
 }
